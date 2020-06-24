e7abe6cf193c667ef9d17ac609ec33558b7c9f00
Fabrice Etanchaud <fabrice.etanchaud@netc.fr>
2020-06-24 16:30:19 +0200

    seed autoboxing

--- dbt/adapters/oracle/impl.py
+++ dbt/adapters/oracle/impl.py
@@ -14,6 +14,27 @@ class OracleAdapter(SQLAdapter):
         return 'CURRENT_DATE'
 
     @classmethod
+    def convert_text_type(cls, agate_table, col_idx):
+        column = agate_table.columns[col_idx]
+        lens = (len(d.encode("utf-8")) for d in column.values_without_nulls())
+        max_len = max(lens) if lens else 64
+        length = max_len if max_len > 16 else 16
+        return "varchar({})".format(length)
+
+    @classmethod
+    def convert_datetime_type(cls, agate_table, col_idx):
+        return "timestamp"
+
+    @classmethod
+    def convert_boolean_type(cls, agate_table, col_idx):
+        return "number(1)"
+
+    @classmethod
     def convert_number_type(cls, agate_table, col_idx):
         decimals = agate_table.aggregate(agate.MaxPrecision(col_idx))
-        return "float" if decimals else "int"
+        return "number"
+
+    @classmethod
+    def convert_time_type(cls, agate_table, col_idx):
+        return "timestamp"
+
