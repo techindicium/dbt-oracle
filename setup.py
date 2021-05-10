ae190495fa6b819f7e45fcf4595a7e58bda5da36
vitor <vitor.avancini@indicium.tech>
2021-05-10 19:34:17 -0300

    Bump

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/techindicium/dbt-oracle',
-    version='0.4.0',
+    version='0.4.1',
     zip_safe=False,
     package_data={
         'dbt': [
